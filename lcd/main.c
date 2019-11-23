30d17f8325bbede6d3050376d0eb7fbe5b097347
yongkal <49707356+yongkal@users.noreply.github.com>
2019-11-23 17:22:34 +0900

    Update main.c
    ..

--- lcd/main.c
+++ lcd/main.c
@@ -1617,7 +1617,7 @@ int sendImage(int sock)
 			}			
 		}
 
-		if(state == 0) //no material on left side, scan right
+		if(state == 0 || lengthmm[2] > 180 ) //no material on left side, scan right
 		{
 			lengthmm[2] = 0;
 			lengthmm[3] = 0;
